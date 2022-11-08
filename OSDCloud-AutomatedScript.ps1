aec110425472b988b331e35dcccc43863b74df3c
ChrisBPON <117733074+ChrisBPON@users.noreply.github.com>
2022-11-08 16:58:20 +0000

    Update OSDCloud-AutomatedScript.ps1
    Adding countdown function to switch between automated script and OSDCloud GUI

--- OSDCloud-AutomatedScript.ps1
+++ OSDCloud-AutomatedScript.ps1
@@ -1,4 +1,8 @@
-﻿Write-Host  -ForegroundColor Cyan "Starting PowerONPlatform's Custom OSDCloud ..."
+## Start PONOSD with default values for internal devices
+
+function Start-PONOSD {
+
+Write-Host  -ForegroundColor Cyan "Starting PowerONPlatform's Custom OSDCloud Script..."
 Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
@@ -22,3 +26,127 @@ Start-OSDCloud -OSName "Windows 11 21H2 x64" -OSEdition Enterprise -OSLanguage e
 Write-Host  -ForegroundColor Cyan "Restarting in 10 seconds!"
 Start-Sleep -Seconds 10
 wpeutil reboot
+
+
+}
+
+
+
+## Countdown function found online with customisations (https://jdhitsolutions.com/blog/powershell/2261/friday-fun-powershell-countdown/)
+
+Function Start-Countdown {
+<# comment based help is in the download version #>
+
+Param(
+[Parameter(Position=0,HelpMessage="Enter seconds to countdown from")]
+[Int]$Seconds = 10,
+[Parameter(Position=1,Mandatory=$False,
+HelpMessage="Enter a scriptblock to execute at the end of the countdown")]
+[scriptblock]$Scriptblock,
+[Switch]$ProgressBar,
+[Switch]$Clear,
+[String]$Message = "Blast Off!"
+)
+
+#save beginning value for total seconds
+$TotalSeconds=$Seconds
+
+If ($clear) {
+Clear-Host
+#find the middle of the current window
+$Coordinate.X=[int]($host.ui.rawui.WindowSize.Width/2)
+$Coordinate.Y=[int]($host.ui.rawui.WindowSize.Height/2)
+}
+
+#define the Escape key
+$ESCKey = 27
+
+#define a variable indicating if the user aborted the countdown
+$Abort=$False
+
+while ($seconds -ge 1) {
+
+if ($host.ui.RawUi.KeyAvailable)
+{
+$key = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyUp,IncludeKeyDown")
+
+if ($key.VirtualKeyCode -eq $ESCkey)
+{
+#ESC was pressed so quit the countdown and set abort flag to True
+$Seconds = 0
+$Abort=$True
+}
+}
+
+If($ProgressBar){
+#calculate percent time remaining, but in reverse so the progress bar
+#moves from left to right
+$percent=100 - ($seconds/$TotalSeconds)*100
+Write-Progress -Activity "Countdown" -SecondsRemaining $Seconds -Status "Time Remaining" -PercentComplete $percent
+Start-Sleep -Seconds 1
+} Else {
+if ($Clear) {
+Clear-Host
+}
+#write the seconds with padded trailing spaces to overwrite any extra digits such
+#as moving from 10 to 9
+$pad=($TotalSeconds -as [string]).Length
+if ($seconds -le 10) {
+$color="Red"
+}
+else {
+$color="Green"
+}
+Write-Host "$(([string]$Seconds).Padright($pad))" -foregroundcolor $color
+Start-Sleep -Seconds 1
+}
+#decrement $Seconds
+$Seconds--
+} #while
+
+if (-Not $Abort) {
+if ($clear) {
+#if $Clear was used, center the message in the console
+$Coordinate.X=$Coordinate.X - ([int]($message.Length)/2)
+}
+
+
+Write-Host "Starting PON OSD with default values" -ForegroundColor Green
+Start-PONOSD
+#run the scriptblock if specified
+if ($scriptblock) {
+Invoke-Command -ScriptBlock $Scriptblock
+}
+}
+else {
+Write-Warning "ESC Key Pressed. Starting OSDCloudGUI..."
+Start-OSDCloudGUI
+}
+} #end function
+
+#define the Escape key
+$ESCKey = 27
+
+#define a variable indicating if the user aborted the countdown
+$Abort=$False
+
+while ($seconds -ge 1) {
+
+if ($host.ui.RawUi.KeyAvailable)
+{
+$key = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyUp,IncludeKeyDown")
+
+if ($key.VirtualKeyCode -eq $ESCkey)
+{
+#ESC was pressed so quit the countdown and set abort flag to True
+$Seconds = 0
+$Abort=$True
+}
+}
+}
+
+
+Write-Host "Welcome to PowerON OSD Cloud!"
+Start-Sleep 1
+Write-Warning "Press ESC within 10 seconds to customise OSDCloud or else default script will run"
+Start-Countdown -ProgressBar -Seconds 10
