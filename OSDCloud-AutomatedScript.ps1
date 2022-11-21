522bf984c38d1a5d4bc9f80c80a6b0a17011d22b
ChrisBPON <117733074+ChrisBPON@users.noreply.github.com>
2022-11-21 23:32:01 +0000

    Update OSDCloud-AutomatedScript.ps1

--- OSDCloud-AutomatedScript.ps1
+++ OSDCloud-AutomatedScript.ps1
@@ -8,7 +8,7 @@ net use Z: \\192.168.1.167\Cats_AP /User:Administrator
 cd Z:
 Register-PSRepository -Name Local -SourceLocation Z:\Packages -InstallationPolicy Trusted
 Find-Module -Repository Local
-Install-Script -Name Get-WindowsAutopilotInfo
+Install-Script -Name Get-WindowsAutopilotInfo -PSRepository Local
 .\Get-WindowsAutopilotInfo.ps1
 copy X:\Autopilot-$Serial.csv Z:
 
