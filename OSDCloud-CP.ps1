fdd1705baf89cd7229d7775275db509ae4a27e3f
ChrisBPON <117733074+ChrisBPON@users.noreply.github.com>
2023-01-20 09:46:04 +0000

    Update OSDCloud-CP.ps1
    Added restart functionality

--- OSDCloud-CP.ps1
+++ OSDCloud-CP.ps1
@@ -3,4 +3,13 @@ Write-Host "This will installed Windows 10 21H2"
 
 Start-Sleep -Seconds 5
 
+Write-Host "At the prompt, press 1 & Enter to choose Windows 10 enterprise"
+
 Start-OSDCloud -FindImageFile -SkipAutopilot -SkipODT -ZTI
+
+Write-Host "Build complete"
+
+#Restart from WinPE
+Write-Host  -ForegroundColor Cyan "Restarting in 5 seconds!"
+Start-Sleep -Seconds 5
+wpeutil reboot
