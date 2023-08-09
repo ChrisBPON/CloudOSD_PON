d62162209227c3f8480b0efdbfe6182d264d6de0
ChrisBPON <117733074+ChrisBPON@users.noreply.github.com>
2023-08-09 15:05:20 +0100

    Update OSDCloud-AutomatedScript.ps1

--- OSDCloud-AutomatedScript.ps1
+++ OSDCloud-AutomatedScript.ps1
@@ -1,168 +1,81 @@
-## Start PONOSD with default values for internal devices
+## Amend this for internal build script e.g. when newer feature update is released - Might be a way to automate pulling through the latest version
+$WindowsOSVersion = "Windows 11 22H2 x64"
 
-function Start-PONOSD {
 
-Write-Host  -ForegroundColor Cyan "Starting PowerONPlatform's Custom OSDCloud Script..."
+function Start-BuildSelection {
+    Write-Host "Which build would you like to run?"
+    Write-Host "1) Internal Build (Automated, will wipe the disk, install $WindowsOSVersion Enterprise, latest drivers and then shutdown"
+    Write-Host "2) Install specific WIM file (Prompt for WIM choice from available WIM files, wipe the disk, install Windows, latest drivers, and then shutdown)"
+    Write-Host "3) Show OSDCloud GUI for manual build"
 
-#Change Display Resolution for Virtual Machine
-if ((Get-MyComputerModel) -match 'Virtual') {
-    Write-Host  -ForegroundColor Cyan "Device is a virtual machine - Setting Display Resolution to 1600x"
-    Set-DisRes 1600
-}
+    $Choice = Read-Host "Please select which build you would like to run:"
 
-#Make sure I have the latest OSD Content -- REMOVED TO SPEED UP THE BUILD TIME
-## Write-Host  -ForegroundColor Cyan "Updating the new OSD PowerShell Module"
-## Install-Module OSD -Force
-
-Write-Host  -ForegroundColor Cyan "Importing the new OSD PowerShell Module"
-Import-Module OSD -Force
-
-#Use Share to install Windows 10 21H2
-net use * \\10.0.5.2\OSDCloud /user:everyone
-Start-OSDCloud -FindImageFile -SkipAutopilot -SkipODT -ZTI
-
-#Start OSDCloud ZTI
-## Write-Host  -ForegroundColor Cyan "Start OSDCloud with default Parameters"
-
-## Start-OSDCloud -OSName "Windows 10 21H2 x64" -OSEdition Enterprise -OSLanguage en-GB -Screenshot -ZTI -SkipAutopilot
-
-#Add reg key to prevent TPM attestation fail
-
-Write-Host -ForegroundColor Cyan "Adding registry key for TPM attestation fix"
-reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\OOBE /v SetupDisplayedEula /t REG_DWORD /d 00000001 /f
-Write-Host -ForegroundColor Cyan "Reg key added!"
-
-Write-Host -ForegroundColor Green "Build complete!"
-Write-Host -ForegroundColor Cyan "Shutting down in 5 seconds!"
-Start-Sleep -Seconds 5
-wpeutil shutdown
+    if ($Choice = 1)
+        {
+            Write-Host "Starting internal build"
+            Start-OSDInternal
+        }
+    elseif ($Choice = 2)
+        {
+            Write-Host "Starting specific WIM file build"
+            Start-OSDWIM
+        }
+    elseif ($Choice = 3)
+        {
+            Write-Host "Starting OSDCloud GUI"
+            Start-OSDCloudGUI
+        }
+    else
+        {
+            Write-Host "Invalid selection. Please try again."
+            Start-BuildSelection
+        }
 
 }
 
+## Functions defined Below ##
 
+function Start-OSDInternal {
 
-## Countdown function found online with customisations (https://jdhitsolutions.com/blog/powershell/2261/friday-fun-powershell-countdown/)
+    ## Install Windows Version listed at top of script, Enterprise, GB language, allow taking screenshots, ZTI so no prompts, skip adding Autopilot profile JSON
+    Start-OSDCloud -OSName "$WindowsOSVersion" -OSEdition Enterprise -OSLanguage en-GB -Screenshot -ZTI -SkipAutopilot
+    Write-Host -ForegroundColor Green "Build complete!"
 
-Function Start-Countdown {
-<# comment based help is in the download version #>
+    ## Fix TPM Attestation issue seen before during Autopilot
+    Start-TPMAttestationFix
 
-Param(
-[Parameter(Position=0,HelpMessage="Enter seconds to countdown from")]
-[Int]$Seconds = 10,
-[Parameter(Position=1,Mandatory=$False,
-HelpMessage="Enter a scriptblock to execute at the end of the countdown")]
-[scriptblock]$Scriptblock,
-[Switch]$ProgressBar,
-[Switch]$Clear,
-[String]$Message = "Blast Off!"
-)
-
-#save beginning value for total seconds
-$TotalSeconds=$Seconds
-
-If ($clear) {
-Clear-Host
-#find the middle of the current window
-$Coordinate.X=[int]($host.ui.rawui.WindowSize.Width/2)
-$Coordinate.Y=[int]($host.ui.rawui.WindowSize.Height/2)
-}
-
-#define the Escape key
-$ESCKey = 27
-
-#define a variable indicating if the user aborted the countdown
-$Abort=$False
-
-while ($seconds -ge 1) {
-
-if ($host.ui.RawUi.KeyAvailable)
-{
-$key = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyUp,IncludeKeyDown")
-
-if ($key.VirtualKeyCode -eq $ESCkey)
-{
-#ESC was pressed so quit the countdown and set abort flag to True
-$Seconds = 0
-$Abort=$True
-}
-}
-
-If($ProgressBar){
-#calculate percent time remaining, but in reverse so the progress bar
-#moves from left to right
-$percent=100 - ($seconds/$TotalSeconds)*100
-Write-Progress -Activity "Countdown" -SecondsRemaining $Seconds -Status "Time Remaining" -PercentComplete $percent
-Start-Sleep -Seconds 1
-} Else {
-if ($Clear) {
-Clear-Host
-}
-#write the seconds with padded trailing spaces to overwrite any extra digits such
-#as moving from 10 to 9
-$pad=($TotalSeconds -as [string]).Length
-if ($seconds -le 10) {
-$color="Red"
-}
-else {
-$color="Green"
-}
-Write-Host "$(([string]$Seconds).Padright($pad))" -foregroundcolor $color
-Start-Sleep -Seconds 1
-}
-#decrement $Seconds
-$Seconds--
-} #while
-
-if (-Not $Abort) {
-if ($clear) {
-#if $Clear was used, center the message in the console
-$Coordinate.X=$Coordinate.X - ([int]($message.Length)/2)
+    Write-Host -ForegroundColor Cyan "Shutting down in 3 seconds!"
+    Start-Sleep -Seconds 3
+    wpeutil shutdown
 }
 
 
-Write-Host "Starting PON OSD with default values" -ForegroundColor Green
-Start-PONOSD
-#run the scriptblock if specified
-if ($scriptblock) {
-Invoke-Command -ScriptBlock $Scriptblock
-}
-}
-else {
-Write-Warning "ESC Key Pressed. Starting OSDCloudGUI..."
-Start-OSDCloudGUI
-}
-} #end function
 
-#define the Escape key
-$ESCKey = 27
+function Start-OSDWIM {
 
-#define a variable indicating if the user aborted the countdown
-$Abort=$False
+    ## Connect to the shared location for WIM files
+    net use * \\10.0.5.2\OSDCloud /user:everyone
 
-while ($seconds -ge 1) {
+    ## Starts OSDCloud with the parameters to search for WIM files, Skip adding Autopilot profile JSON, Skip ODT, and Zero Touch Installation (No prompts throughout build process)
+    Start-OSDCloud -FindImageFile -SkipAutopilot -SkipODT -ZTI
+    Write-Host -ForegroundColor Green "Build complete!"
 
-if ($host.ui.RawUi.KeyAvailable)
-{
-$key = $host.ui.RawUI.ReadKey("NoEcho,IncludeKeyUp,IncludeKeyDown")
+    ## Fix TPM Attestation issue seen before during Autopilot
+    Start-TPMAttestationFix
 
-if ($key.VirtualKeyCode -eq $ESCkey)
-{
-#ESC was pressed so quit the countdown and set abort flag to True
-$Seconds = 0
-$Abort=$True
-}
-}
+    Write-Host -ForegroundColor Cyan "Shutting down in 3 seconds!"
+    Start-Sleep -Seconds 3
+    wpeutil shutdown
 }
 
 
-Write-Host "Welcome to PowerON OSD Cloud!"
-Start-Sleep 1
-Write-Warning "Press ESC within 10 seconds to customise OSDCloud or else default script will run"
-Write-Host "`n"
-Write-Host "`n"
-Write-Host "`n"
 
-## Use below for automation
-## Start-Countdown -ProgressBar -Seconds 10 -Clear
+function Start-TPMAttestationFix {
 
-Start-PONOSD
+    Write-Host -ForegroundColor Cyan "Adding registry key for TPM attestation fix"
+    reg add HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Setup\OOBE /v SetupDisplayedEula /t REG_DWORD /d 00000001 /f
+    Write-Host -ForegroundColor Cyan "Reg key added!"
+}
+
+Write-Host "PowerON Automated Building Process V1.0" -ForegroundColor Yellow
+Start-BuildSelection
