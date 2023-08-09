9c6b5458d9738dcc8fb1dfc58b9247e20402a582
ChrisBPON <117733074+ChrisBPON@users.noreply.github.com>
2023-08-09 17:08:14 +0100

    Update OSDCloud-AutomatedScript.ps1

--- OSDCloud-AutomatedScript.ps1
+++ OSDCloud-AutomatedScript.ps1
@@ -1,14 +1,6 @@
 ## Amend this for internal build script e.g. when newer feature update is released - Might be a way to automate pulling through the latest version
 $WindowsOSVersion = "Windows 10 21H2 x64"
 
-function Start-UpdateOSD {
-
-Write-Host "Updating OSD"
-Install-Module OSD -Force
-Import-Module OSD -Force
-
-}
-
 function Start-BuildSelection {
     Write-Host "Which build would you like to run?"
     Write-Host "1) Internal Build (Automated, will wipe the disk, install $WindowsOSVersion Enterprise, latest drivers and then shutdown"
@@ -92,6 +84,5 @@ function Start-TPMAttestationFix {
     Write-Host -ForegroundColor Cyan "Reg key added!"
 }
 
-Start-UpdateOSD
 Write-Host "PowerON Automated Building Process V1.0" -ForegroundColor Yellow
 Start-BuildSelection
